8d24972fbeafcc381369d99b1dd842696bccf097
brianhotopp <brihoto@gmail.com>
2021-03-12 22:38:50 -0500

    all scrapers are functional

--- scrapers/rivalry/scraper.py
+++ scrapers/rivalry/scraper.py
@@ -12,10 +12,10 @@ from utils import transcribe_table_data, postgres_db_insert
 ENVIRONMENT = os.environ['ENVIRONMENT']
 SENTRY_URL = os.environ['SENTRY_URL']
 DB_CREDENTIALS = {
-	'host': os.environ['DB_HOST'],
-	'user': os.environ['DB_USER'],
-	'password': os.environ['DB_PASSWORD'],
-	'dbname': os.environ['DB_NAME']
+        'host': os.environ['DB_HOST'],
+        'user': os.environ['DB_USER'],
+        'password': os.environ['DB_PASSWORD'],
+        'dbname': os.environ['DB_NAME']
 }
 
 
@@ -23,36 +23,35 @@ DB_CREDENTIALS = {
 logging.config.dictConfig(LOGGING)
 logger = logging.getLogger(ENVIRONMENT)
 if ENVIRONMENT == 'PRODUCTION':
-	sentry_sdk.init(SENTRY_URL)
+        sentry_sdk.init(SENTRY_URL)
 
 
 if __name__ == '__main__':
 
-	logger.info('Starting scrape job for rivalry table data.')
+        logger.info('Starting scrape job for rivalry table data.')
 
-	# initialize headless selenium webdriver
-	chrome_options = webdriver.ChromeOptions()
-	chrome_options.add_argument('--headless')
-	chrome_options.add_argument('--no-sandbox')
-	chrome_options.add_argument('--disable-dev-shm-usage')
-	driver = webdriver.Chrome(chrome_options=chrome_options)
+        # initialize headless selenium webdriver
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(chrome_options=chrome_options)
 
-	# load website / raw table data
-	driver.get(RIVALRY_URL)
-	time.sleep(5)  # give webpage time to load table
-	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # scroll down to load dynamic content
-	time.sleep(1)
-	table = driver.find_element_by_id('__nuxt')
-	table = table.text.split('\n')
-	table = transcribe_table_data(table)
+        # load website / raw table data
+        driver.get(RIVALRY_URL)
+        time.sleep(5)  # give webpage time to load table
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # scroll down to load dynamic content
+        time.sleep(1)
+        table = driver.find_element_by_id('__nuxt').get_attribute("innerHTML")
+        table = transcribe_table_data(table)
 
-	logger.info('Finished processing of %s rows.', len(table))
+        logger.info('Finished processing of %s rows.', len(table))
 
-	# insert to db
-	if ENVIRONMENT == 'PRODUCTION' and len(table) > 0:
-		logger.info('Inserting %s rows into database.', len(table))
-		postgres_db_insert(table, DB_CREDENTIALS)
-	elif len(table) == 0:
-		logger.warning('EGB data scrape produced 0 data points.')
-	else:
-		logger.info('Produced data: %s', table)
+        # insert to db
+        if ENVIRONMENT == 'PRODUCTION' and len(table) > 0:
+                logger.info('Inserting %s rows into database.', len(table))
+                postgres_db_insert(table, DB_CREDENTIALS)
+        elif len(table) == 0:
+                logger.warning('EGB data scrape produced 0 data points.')
+        else:
+                logger.info('Produced data: %s', table)
